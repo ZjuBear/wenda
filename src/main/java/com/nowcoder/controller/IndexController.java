dec504b9e394a8c3e42b5135a47805f71e77519e
76912 <769127490@qq.com>
2018-03-21 10:09:07 +0800

    Idea整合GitHub测试

--- src/main/java/com/nowcoder/controller/IndexController.java
+++ src/main/java/com/nowcoder/controller/IndexController.java
@@ -36,14 +36,6 @@ public class IndexController {
         return wendaService.getMessage(2) + "Hello NowCoder" + httpSession.getAttribute("msg");
     }
 
-    @RequestMapping(path = {"/profile/{groupId}/{userId}"})
-    @ResponseBody
-    public String profile(@PathVariable("userId") int userId,
-                          @PathVariable("groupId") String groupId,
-                          @RequestParam(value = "type", defaultValue = "1") int type,
-                          @RequestParam(value = "key", required = false) String key) {
-        return String.format("Profile Page of %s / %d, t:%d k: %s", groupId, userId, type, key);
-    }
 
     @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
     public String template(Model model) {
